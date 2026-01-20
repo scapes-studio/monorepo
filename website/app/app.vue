ebc2b33239844914a2e33dcd1ef6c022f2dba8ed
jalil <jalil@1001.digital>
2026-01-20 14:58:05 +0100

    Always show grid in background

--- website/app/app.vue
+++ website/app/app.vue
@@ -15,4 +15,16 @@ useScapeGrid()
 :root {
   color-scheme: light;
 }
+
+body {
+  background-color: white;
+  background-image:
+    linear-gradient(to right, #e5e5e5 var(--grid-gutter), transparent var(--grid-gutter)),
+    linear-gradient(to bottom, #e5e5e5 var(--grid-gutter), transparent var(--grid-gutter));
+  background-size:
+    calc(var(--scape-width) + var(--grid-gutter)) calc(var(--scape-height) + var(--grid-gutter));
+  background-position: 0 0;
+
+  min-height: 100dvh;
+}
 </style>
