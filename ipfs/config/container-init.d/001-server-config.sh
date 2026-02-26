eeab4abc38694da44edddfb54fc2fd639f13264c
jalil <jalil@1001.digital>
2026-02-27 00:54:47 +0100

    Enable decentralized responses

--- ipfs/config/container-init.d/001-server-config.sh
+++ ipfs/config/container-init.d/001-server-config.sh
@@ -11,6 +11,9 @@ ipfs config Addresses.API /ip4/127.0.0.1/tcp/5001
 # Only serve content that is already pinned/cached locally
 ipfs config --bool Gateway.NoFetch true
 
+# Enable directory listings and other deserialized responses
+ipfs config --bool Gateway.DeserializedResponses true
+
 # Allow the admin WebUI and standard origins to access the RPC API
 ORIGINS='["https://'"${IPFS_ADMIN_HOST}"'", "http://localhost:3000", "http://127.0.0.1:5001", "https://webui.ipfs.io"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "$ORIGINS"
