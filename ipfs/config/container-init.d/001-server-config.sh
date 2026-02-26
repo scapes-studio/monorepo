21cff0c8fe7c7aff8d96916b6a301adadf42c95a
jalil <jalil@1001.digital>
2026-02-27 00:30:03 +0100

    Allow CORS from webui

--- ipfs/config/container-init.d/001-server-config.sh
+++ ipfs/config/container-init.d/001-server-config.sh
@@ -10,3 +10,8 @@ ipfs config Addresses.API /ip4/127.0.0.1/tcp/5001
 
 # Only serve content that is already pinned/cached locally
 ipfs config --bool Gateway.NoFetch true
+
+# Allow the admin WebUI and standard origins to access the RPC API
+ORIGINS='["https://'"${IPFS_ADMIN_HOST}"'", "http://localhost:3000", "http://127.0.0.1:5001", "https://webui.ipfs.io"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "$ORIGINS"
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]'
